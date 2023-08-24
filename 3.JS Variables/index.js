8a2b4577e057d1da0218d5f6f4aa8714b564b5d3
Yuvraj Kori <yuvrajkori29@gmail.com>
2023-08-24 15:15:00 +0530

    24-8-2023

---
+++ 3.JS Variables/index.js
@@ -0,0 +1,10 @@
+const inputs = document.querySelectorAll('.controls input');
+
+function handleUpdate(){
+    // console.log(this.value);
+    const suffix = this.dataset.sizing || ' ';
+    // console.log(this.name);
+    document.documentElement.style.setProperty(`--${this.name}`,this.value + suffix);
+}
+
+inputs.forEach(input => input.addEventListener('change',handleUpdate));
