dff7bbb790278efd312192efb0ca72d9d2250a2e
Yuvraj Kori <yuvrajkori29@gmail.com>
2023-09-04 22:45:59 +0530

    KEy up challenge

---
+++ 12.Key Sequence/index.js
@@ -0,0 +1,22 @@
+
+const pressed = [];
+const secretCode = 'wesbos';
+
+
+
+
+window.addEventListener('keyup',(e)=>{
+    console.log(e.key);
+    pressed.push(e.key);
+    console.log(pressed);
+
+    pressed.splice(-secretCode.length-1 ,pressed.length-secretCode.length);
+
+    if(pressed.join('').includes(secretCode))
+    {
+console.log("DING DING");
+cornify_add();
+    }
+
+    console.log(pressed);
+})
